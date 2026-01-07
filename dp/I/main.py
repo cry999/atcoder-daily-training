1adae2f3a579a7be486f590a71e0040b46147022
Cry999 <when.the.cry999@gmail.com>
2026-01-07 20:49:10 +0900

    dp i 2nd

--- dp/I/main.py
+++ dp/I/main.py
@@ -1,14 +1,12 @@
 N = int(input())
 (*p,) = map(float, input().split())
-p = [0.0] + p
+# dp[j] = (i-1 枚目までの裏表が決まっているとして) i 枚目の裏表が決まったときに j 枚が表の確率
+dp = [0] * (N + 1)
+dp[0] = 1
 
-dp = [[0.0] * (N + 1) for _ in range(N + 1)]
-dp[0][0] = 1.0
-
-for i in range(1, N + 1):
-    for j in range(i + 1):
-        dp[i][j] = dp[i - 1][j] * (1 - p[i])
+for i in range(N):
+    for j in range(i + 1, -1, -1):
+        dp[j] *= 1 - p[i]
         if j > 0:
-            dp[i][j] += dp[i - 1][j - 1] * p[i]
-
-print(sum(dp[N][(N + 1) // 2 :]))
+            dp[j] += dp[j - 1] * p[i]
+print(sum(dp[N // 2 + 1 :]))
