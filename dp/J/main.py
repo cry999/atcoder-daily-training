03a3cabd617e38026462763ff594d610da447a87
Cry999 <when.the.cry999@gmail.com>
2026-01-10 19:14:35 +0900

    dp J 2nd

--- dp/J/main.py
+++ dp/J/main.py
@@ -1,40 +1,49 @@
-import sys
-
-sys.setrecursionlimit(10**7)
-
-
 N = int(input())
 (*a,) = map(int, input().split())
 
+N1, N2, N3 = [a.count(i + 1) for i in range(3)]
+dp = [[[-1] * (N + 1) for _ in range(N + 1)] for _ in range(N + 1)]
+dp[N1][N2][N3] = 1
 
-states = [[[-1] * (N + 1) for _ in range(N + 1)] for _ in range(N + 1)]
+
+def ok(n1: int, n2: int, n3: int) -> bool:
+    """n1, n2, n3 があり得る状態かを判定する"""
+    if n3 > N3:
+        return False
+    if n3 == N3 and n2 > N2:
+        return False
+    if n3 == N3 and n2 == N2 and n1 > N1:
+        return False
+    return 0 <= n1 and 0 <= n2 and 0 <= n3 and n3 + n2 + n1 <= N
 
 
 def dfs(n1: int, n2: int, n3: int) -> float:
-    global states
+    if dp[n1][n2][n3] != -1:
+        return dp[n1][n2][n3]
 
-    if n1 == n2 == n3 == 0:
-        states[n1][n2][n3] = 0
-        return states[n1][n2][n3]
-    if n2 == n3 == 0 and n1 == 1:
-        states[n1][n2][n3] = N
-        return states[n1][n2][n3]
+    dp[n1][n2][n3] = 0
+    if ok(n1 + 1, n2, n3):
+        # 1 の皿から 0 の皿へ
+        dp[n1][n2][n3] += (n1 + 1) * dfs(n1 + 1, n2, n3) / N
+    if ok(n1 - 1, n2 + 1, n3):
+        # 2 の皿から 1 の皿へ
+        dp[n1][n2][n3] += (n2 + 1) * dfs(n1 - 1, n2 + 1, n3) / N
+    if ok(n1, n2 - 1, n3 + 1):
+        # 3 の皿から 2 の皿へ
+        dp[n1][n2][n3] += (n3 + 1) * dfs(n1, n2 - 1, n3 + 1) / N
 
-    if states[n1][n2][n3] != -1:
-        return states[n1][n2][n3]
-
-    ans = 0
-    if n1 > 0:
-        ans += dfs(n1 - 1, n2, n3) * n1
-    if n2 > 0:
-        ans += dfs(n1 + 1, n2 - 1, n3) * n2
-    if n3 > 0:
-        ans += dfs(n1, n2 + 1, n3 - 1) * n3
-    ans += N
-    ans /= n1 + n2 + n3
-
-    states[n1][n2][n3] = ans
-    return states[n1][n2][n3]
+    if n1 + n2 + n3:
+        dp[n1][n2][n3] *= N / (n1 + n2 + n3)
+    return dp[n1][n2][n3]
 
 
-print(dfs(*[a.count(i + 1) for i in range(3)]))
+dfs(0, 0, 0)
+
+ans = 0
+for n1 in range(N + 1):
+    for n2 in range(N + 1):
+        for n3 in range(N + 1):
+            if dp[n1][n2][n3] == -1:
+                continue
+            ans += dp[n1][n2][n3]
+print(ans-1)
