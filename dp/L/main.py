2e77e0d89cab9a91e2d072c1a47b9eff92a0fb18
Cry999 <when.the.cry999@gmail.com>
2026-01-10 19:55:01 +0900

    dp L 2nd

--- dp/L/main.py
+++ dp/L/main.py
@@ -1,15 +1,18 @@
 N = int(input())
 (*a,) = map(int, input().split())
 
-# dp[l][r] := [l, r] の範囲の数字が残っている時に手番が回ってきた方から
-# みた、得点差の最大値
-dp = [[float("inf")] * N for _ in range(N)]
+dp = [[-float("inf")] * N for _ in range(N)]
+
 for i in range(N):
     dp[i][i] = a[i]
 
 for d in range(1, N):
-    for l in range(N - d):
+    for l in range(N):
         r = l + d
-        dp[l][r] = max(a[l] - dp[l + 1][r], a[r] - dp[l][r - 1])
-
+        if r >= N:
+            continue
+        dp[l][r] = max(
+            a[l] - dp[l + 1][r] if l + 1 <= r else 0,
+            a[r] - dp[l][r - 1] if l <= r - 1 else 0,
+        )
 print(dp[0][-1])
