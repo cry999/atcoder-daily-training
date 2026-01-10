995c40aa40cde1a41e9534ea0d027df19b707969
Cry999 <when.the.cry999@gmail.com>
2026-01-10 20:37:45 +0900

    dp M 2nd

--- dp/M/main.py
+++ dp/M/main.py
@@ -1,19 +1,19 @@
-MOD = 10**9 + 7
-
 N, K = map(int, input().split())
 (*a,) = map(int, input().split())
+
+MOD = 10**9 + 7
 
 dp = [[0] * (K + 1) for _ in range(N + 1)]
 dp[0][0] = 1
 
 for i in range(N):
-    for k in range(K + 1):
-        if k == 0:
-            dp[i + 1][k] = dp[i][k]
-        elif k - a[i] <= 0:
-            dp[i + 1][k] = dp[i + 1][k - 1] + dp[i][k]
-        else:
-            dp[i + 1][k] = dp[i + 1][k - 1] + (dp[i][k] - dp[i][k - a[i] - 1]) % MOD
-        dp[i + 1][k] %= MOD
+    ai = a[i]
 
-print(dp[-1][K])
+    dp[i + 1][0] = dp[i][0]
+    for k in range(K):
+        dp[i + 1][k + 1] = dp[i + 1][k] + dp[i][k + 1]
+        if k >= ai:
+            dp[i + 1][k + 1] -= dp[i][k - ai]
+        dp[i + 1][k + 1] %= MOD
+
+print(dp[-1][-1])
