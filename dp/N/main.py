36ff4178d696061a2c815a37c11b356e77b7e82d
Cry999 <when.the.cry999@gmail.com>
2026-01-10 20:53:18 +0900

    dp n 2nd

--- dp/N/main.py
+++ dp/N/main.py
@@ -1,36 +1,25 @@
-import sys
-
-sys.setrecursionlimit(10**7)
-
-
 N = int(input())
 (*a,) = map(int, input().split())
 
-dp = [[-1] * (N + 1) for _ in range(N + 1)]
-c = [0] * (N + 1)
+dp = [[float("inf")] * N for _ in range(N)]
+cum = [0] * (N + 1)
 for i in range(N):
-    c[i + 1] = c[i] + a[i]
+    cum[i + 1] = cum[i] + a[i]
 
 
-def dfs(l: int, r: int) -> int:
-    if r < l:
+def dfs(l: int, r: int):
+    if l > r:
         return float("inf")
-
-    if dp[l][r] != -1:
+    if l == r:
+        return 0
+    if dp[l][r] != float("inf"):
         return dp[l][r]
-
-    if r == l:
-        dp[l][r] = 0
-        return dp[l][r]
-
-    if r == l + 1:
+    if r - l == 1:
         dp[l][r] = a[l] + a[r]
         return dp[l][r]
 
-    dp[l][r] = float("inf")
-    for i in range(l, r):
-        dp[l][r] = min(dp[l][r], dfs(l, i) + dfs(i + 1, r))
-    dp[l][r] += c[r + 1] - c[l]
+    for m in range(l, r):
+        dp[l][r] = min(dp[l][r], dfs(l, m) + dfs(m + 1, r) + cum[r + 1] - cum[l])
     return dp[l][r]
 
 
