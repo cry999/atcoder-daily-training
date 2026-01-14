f62c3ed62718f42e564018f6f2c5086a6b4aff97
Cry999 <when.the.cry999@gmail.com>
2026-01-15 00:44:17 +0900

    dp O 2nd

--- dp/O/main.py
+++ dp/O/main.py
@@ -1,21 +1,23 @@
-MOD = 10**9 + 7
 N = int(input())
-a = [tuple(map(int, input().split())) for _ in range(N)]
+MOD = 10**9 + 7
 
-# dp[i][S] := i 番目までの男性を S に含まれる女性とペアにする場合の数
+(*a,) = [list(map(int, input().split())) for _ in range(N)]
+
+# dp[i][j]: i 番目の男性までをマッチングできるか判断した時の女性のマッチング状況が j である場合の数
 dp = [[0] * (1 << N) for _ in range(N + 1)]
 dp[0][0] = 1
 
 for i in range(N):
-    for s in range(1 << N):
-        if dp[i][s] == 0:
+    for j in range(1 << N):
+        if dp[i][j] == 0:
             continue
-        for j in range(N):
-            if s & (1 << j):
+
+        for k in range(N):
+            if j & (1 << k):
                 continue
-            if a[i][j] == 0:
+            if a[i][k] == 0:
                 continue
-            dp[i + 1][s | (1 << j)] += dp[i][s]
-            dp[i + 1][s | (1 << j)] %= MOD
+            dp[i + 1][j | (1 << k)] += dp[i][j]
+            dp[i + 1][j | (1 << k)] %= MOD
 
 print(dp[-1][-1])
