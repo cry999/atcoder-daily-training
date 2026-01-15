c29007d8bb34739bfa328cd390e0dfd62ef5518a
Cry999 <when.the.cry999@gmail.com>
2026-01-16 01:11:48 +0900

    dp P 2nd

--- dp/P/main.py
+++ dp/P/main.py
@@ -2,32 +2,33 @@ import sys
 
 sys.setrecursionlimit(10**7)
 
-MOD = 10**9 + 7
-
 
 N = int(input())
-tree = [[] for _ in range(N + 1)]
+MOD = 10**9 + 7
+
+graph = [[] for _ in range(N)]
 
 for _ in range(N - 1):
     x, y = map(int, input().split())
-    tree[x].append(y)
-    tree[y].append(x)
+    x, y = x - 1, y - 1
+
+    graph[x].append(y)
+    graph[y].append(x)
 
 
 def dfs(u: int, parent: int) -> tuple[int, int]:
-    black, white = 1, 1
-    for v in tree[u]:
+    """u が黒の時の場合の数と白の時の場合の数を返す。"""
+    b, w = 1, 1
+    for v in graph[u]:
         if v == parent:
             continue
-        nb, nw = dfs(v, u)
-
-        black *= nw
-        black %= MOD
-
-        white *= nb + nw
-        white %= MOD
-
-    return black, white
+        cb, cw = dfs(v, u)
+        b *= cw
+        b %= MOD
+        w *= cb + cw
+        w %= MOD
+    return b, w
 
 
-print(sum(dfs(1, -1)) % MOD)
+b, w = dfs(0, -1)
+print((b + w) % MOD)
