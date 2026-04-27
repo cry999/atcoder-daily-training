75714a05cafda6571e237b90b61d27138b84852c
Cry999 <when.the.cry999@gmail.com>
2026-04-27 16:20:10 +0900

    adt: 2026/04/01 1600 challenge again

---
+++ adt/2026/04/01/1600/g1.py
@@ -0,0 +1,29 @@
+N, M = map(int, input().split())
+g = [[] for _ in range(N)]
+
+for _ in range(M):
+    a, b, w = map(int, input().split())
+    g[a - 1].append((b - 1, w))
+
+# visited[i][w]: 頂点 i に重み w で訪れることができるか
+visited = [[False] * (1 << 10) for _ in range(N)]
+visited[0][0] = True
+
+q = [(0, 0)]
+while q:
+    u, w = q.pop()
+
+    for v, nw in g[u]:
+        nw ^= w
+        if visited[v][nw]:
+            continue
+        visited[v][nw] = True
+
+        q.append((v, nw))
+
+for w in range(1 << 10):
+    if visited[N - 1][w]:
+        print(w)
+        break
+else:
+    print(-1)
